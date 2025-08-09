4bbe0f61e9476a1abebe08e69cf37e7d7a7b9f8e
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-08-09 01:24:51 +0000

    progress as of end of class 8-8

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,23 +1,37 @@
 import React, { useEffect, useState } from "react";
 import Counter from "./Counter.jsx";
 
+// REMINDER: ANNOTATE HOW CODE WORKS FOR FUTURE SELF
+
 //create your first component
 const Home = ({ time }) => {
   const stringTime = time.toString();
   console.log(stringTime + 1);
+  const digit1 = time < 10 ? stringTime[0] : stringTime[1];
+  const digit2 = time < 10 ? "0" : stringTime[0];
 
-// 12 --> [0][1]
+// condition ? do this : do that;          THIS IS A TERNARY OPERATOR
+
+// 0 1 2 POSITIONS
+// ---------------
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 10
+// 13
 
   return (
     <div className="text-center">
-      <span className="digit1 border bg-secondary p-3 m-2">{stringTime[0]}</span>
-      <span className="digit2 border bg-secondary p-3 m-2">{stringTime[1]}</span>
-      <span className="digit2 border bg-secondary p-3 m-2">{stringTime[2]}</span>
-      <span className="digit2 border bg-secondary p-3 m-2">{stringTime[3]}</span>
-      <span className="digit2 border bg-secondary p-3 m-2">{stringTime[4]}</span>
-      <span className="digit2 border bg-secondary p-3 m-2">{stringTime[5]}</span>
+      <span className="digit1 border bg-secondary p-3 m-2">{digit2}</span>
+      <span className="digit2 border bg-secondary p-3 m-2">{digit1}</span>
     </div>
   );
 };
 
 export default Home;
+
+//{stringTime[0]}
